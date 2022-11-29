2145b7b57ddf247e23fb6fcc2c5ae6e5aaac15a5
Gupuroom <cjsrnr1114@gmail.com>
2022-11-29 11:03:10 +0900

    ExceptionController 수정 - 알 수 없는 에러 -> 자세한 에러 원인 파악

--- src/main/java/com/bucwith/common/CommController.java
+++ src/main/java/com/bucwith/common/CommController.java
@@ -25,4 +25,14 @@ public class CommController  {
         return ResponseEntity.ok().body(
                 ResponseDto.builder().code(apiCode.getCode()).codeMsg(apiCode.getMsg()).data(data).build());
     }
+
+    public ResponseEntity ErrorReturn(int code, String msg) {
+        return ResponseEntity.ok().body(
+                ResponseDto.builder().code(code).codeMsg(msg).build());
+    }
+
+    public ResponseEntity ErrorReturn(int code, String msg, Object data) {
+        return ResponseEntity.ok().body(
+                ResponseDto.builder().code(code).codeMsg(msg).data(data).build());
+    }
 }
