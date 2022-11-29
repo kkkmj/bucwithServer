1489ad4d1b16de890504fa2f77880ef5961e9b24
kkkmj <wjsdus531@naver.com>
2022-11-30 00:38:49 +0900

    잘못 올라간 커밋파일 수정

--- src/main/java/com/bucwith/common/CommController.java
+++ src/main/java/com/bucwith/common/CommController.java
@@ -26,5 +26,13 @@ public class CommController  {
                 ResponseDto.builder().code(apiCode.getCode()).codeMsg(apiCode.getMsg()).data(data).build());
     }
 
+    public ResponseEntity ErrorReturn(int code, String msg) {
+        return ResponseEntity.ok().body(
+                ResponseDto.builder().code(code).codeMsg(msg).build());
+    }
 
+    public ResponseEntity ErrorReturn(int code, String msg, Object data) {
+        return ResponseEntity.ok().body(
+                ResponseDto.builder().code(code).codeMsg(msg).data(data).build());
+    }
 }
