54571e553664a45b63275b7a00a9ff9ed9935226
Gupuroom <cjsrnr1114@gmail.com>
2022-12-06 10:19:55 +0900

    Star, Icon 수정 - 하나의 메서드가 하나의 동작만을 하도록 수정 - 객체지향적 코드 지향 (Icon 조회는 Icon 객체에서 해야함.) - 클린 코드 지향

--- src/main/java/com/bucwith/service/star/StarService.java
+++ src/main/java/com/bucwith/service/star/StarService.java
@@ -1,11 +1,10 @@
 package com.bucwith.service.star;
 
 
-import com.bucwith.domain.icon.Icon;
 import com.bucwith.domain.star.Star;
 import com.bucwith.dto.star.StarReqDto;
-import com.bucwith.repository.icon.IconRepository;
 import com.bucwith.repository.star.StarRepository;
+import com.bucwith.service.icon.IconService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -21,25 +20,22 @@ import java.util.List;
 public class StarService {
 
     private final StarRepository starRepository;
-    private final IconRepository iconRepository;
+    private final IconService iconService;
 
     public Star register(StarReqDto reqDto) {
-        Icon icon = iconRepository.findByIconCode(reqDto.getIconCode())
-                .orElseThrow(() -> new IllegalArgumentException("해당 Icon이 없습니다. code=" + reqDto.getIconCode()));
-
-        return starRepository.save(reqDto.toEntity(icon));
+        return starRepository.save(reqDto.toEntity(iconService.getIcon(reqDto.getIconCode())));
     }
 
-    public Star findStarById(int starId){
+    public void remove(int starId) {
+        starRepository.deleteById(starId);
+    }
+
+    public Star getStar(int starId) {
         return starRepository.findById(starId)
                 .orElseThrow(() -> new NullPointerException("해당 Star가 없습니다. starId=" + starId));
     }
 
-    public List<Star> findStarByBucketId(int bucketId){
+    public List<Star> getStarByBucketId(int bucketId) {
         return starRepository.findByBucketId(bucketId);
-    }
-
-    public void remove(int starId){
-        starRepository.deleteById(starId);
     }
 }
