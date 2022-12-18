09bdae1e7e29044682769fa661e7245a6622b82d
kkkmj <wjsdus531@naver.com>
2022-12-18 21:07:41 +0900

    filter두번 되는거 수정

--- src/main/java/com/bucwith/common/config/oauth/JwtExceptionFilter.java
+++ src/main/java/com/bucwith/common/config/oauth/JwtExceptionFilter.java
@@ -32,18 +32,23 @@ public class JwtExceptionFilter extends OncePerRequestFilter {
         } catch (SecurityException | MalformedJwtException e) {
             log.error("Invalid JWT signature: {}", e.getMessage());
             request.setAttribute("exception", WRONG_TYPE_TOKEN.getCode());
+            filterChain.doFilter(request, response);
         } catch (ExpiredJwtException e) {
             log.error("JWT token is expired: {}", e.getMessage());
             request.setAttribute("exception", EXPIRED_TOKEN.getCode());
+            filterChain.doFilter(request, response);
         } catch (UnsupportedJwtException e) {
             request.setAttribute("exception", UNSUPPORTED_TOKEN.getCode());
             log.error("JWT token is unsupported: {}", e.getMessage());
+            filterChain.doFilter(request, response);
         } catch (IllegalArgumentException e) {
             request.setAttribute("exception", EMPTY_JWT.getCode());
             log.error("JWT claims string is empty: {}", e.getMessage());
+            filterChain.doFilter(request, response);
         } catch (SignatureException e){
             request.setAttribute("exception", INVALID_JWT.getCode());
             log.error("Invalid JWT signature: {}", e.getMessage());
+            filterChain.doFilter(request, response);
         }
         catch (Exception e) {
             log.error("================================================");
@@ -55,8 +60,8 @@ public class JwtExceptionFilter extends OncePerRequestFilter {
             log.error("}");
             log.error("================================================");
             request.setAttribute("exception", UNKNOWN_ERROR.getCode());
+            filterChain.doFilter(request, response);
         }
-
     }
 }
 
